bef7ffc22dffc5fc04dab4e17a3328525ba582f1
Apu <macbookpro@MacBooks-MacBook-Pro.local>
2022-07-15 13:52:58 +0600

    contact page fix

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -18,21 +18,21 @@ function Contact() {
                 <a href="https://www.linkedin.com/company/bracudichari/" target="_blank"><FaLinkedin className='h-12 w-12 text-white'/></a>
             </div>
 
-            <iframe className="border-2 rounded h-2/3 w-2/3 mt-10"src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14604.321425585482!2d90.407157!3d23.7801527!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x210f65a620d53ea8!2sBRACU%20Dichari!5e0!3m2!1sen!2sbd!4v1657818339190!5m2!1sen!2sbd"   loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe className="border-2 rounded h-2/3 w-2/3 mt-10"src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14604.321425585482!2d90.407157!3d23.7801527!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x210f65a620d53ea8!2sBRACU%20Dichari!5e0!3m2!1sen!2sbd!4v1657818339190!5m2!1sen!2sbd"   loading="lazy" ></iframe>
             </div>
         </div>
         <div className="contact-form-wrapper">
             <form>
             <div className="form-item">
-                <input type="text" name="sender" required/>
+                <input type="text" name="sender" placeholder='Full Name' required/>
                 <label>Name:</label>
             </div>
             <div className="form-item">
-                <input type="text" name="email" required/>
+                <input type="text" name="email" placeholder="xyz@mail.com" required/>
                 <label>Email:</label>
             </div>
             <div className="form-item">
-                <textarea className="" name="message" required></textarea>
+                <textarea className="" name="message" placeholder="Message ......"required></textarea>
                 <label>Message:</label>
             </div>
             <button className="submit-btn">Send</button>  
